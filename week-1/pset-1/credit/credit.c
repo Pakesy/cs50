7305a3f19b9c62772982fad278aeda7056f0bc9f
pakesy <conorpakes@gmail.com>
2023-06-01 20:08:11 +1000

    Hello & Mario more

---
+++ week-1/pset-1/credit/credit.c
@@ -0,0 +1,13 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void luhn(long num);
+
+int main(void) 
+{
+    
+}
+
+void luhn(long num) {
+    for(int i = 0; i < num.length)
+}
--- week-1/pset-1/mario-more/mario.c
+++ week-1/pset-1/mario-more/mario.c
@@ -1,7 +1,58 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+void pyramid(int size);
+void clearInputBuffer();
+
+int main(void)
 {
-    
+    int height;
+    printf("Height: ");
+    while (scanf("%d", &height) != 1 || height < 1 || height > 8)
+    {
+        clearInputBuffer();
+        printf("Height: ");
+    }
+
+    pyramid(height);
+
+    return 0;
+}
+
+void clearInputBuffer()
+{
+    int c;
+    while ((c = getchar()) != '\n' && c != EOF)
+    {
+    }
+}
+
+void pyramid(int size)
+{
+    for (int i = 1; i <= size; i++)
+    {
+        // Print spaces for the left side of the pyramid
+        for (int j = size - i; j > 0; j--)
+        {
+            printf(" ");
+        }
+
+        // Print # symbols for the left side of the pyramid
+        for (int j = 0; j < i; j++)
+        {
+            printf("#");
+        }
+
+        // Print spacing between the two sides of the pyramid
+        printf("  ");
+
+        // Print # symbols for the right side of the pyramid
+        for (int j = 0; j < i; j++)
+        {
+            printf("#");
+        }
+
+        // Move to the next line for the next row
+        printf("\n");
+    }
 }
